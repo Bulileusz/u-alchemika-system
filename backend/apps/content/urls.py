2036b5da4d0627f02b2b988d14249aa5ad898c27
Maciej Bułhak <maciek.bulhak@gmail.com>
2026-06-05 22:10:37 +0200

    feat: faza 3 - blog i atrakcje okolicy (Osoba C)

--- backend/apps/content/urls.py
+++ backend/apps/content/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from . import views
 
 app_name = 'content'
 
 urlpatterns = [
   # --- Osoba C: blog i atrakcje ---
-    # path('blog/', views.post_list, name='post_list'),
-    # path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
-    # path('atrakcje/', views.attraction_list, name='attraction_list'),
+    path('blog/', views.post_list, name='post_list'),
+    path('blog/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('atrakcje/', views.attraction_list, name='attraction_list'),
 ]
+

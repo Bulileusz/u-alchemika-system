83a3d5483f131b87d7714486ef77b27d111c6218
Malgorzata Bulhak <malgorzata.bulhak@studenci.collegiumwitelona.pl>
2026-06-05 18:18:43 +0200

    feat: add rooms pages

--- backend/apps/core/views/rooms.py
+++ backend/apps/core/views/rooms.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from ..models import Room
 
 def index(request):
-    return render(request, 'index.html')
+    featured_rooms = (
+        Room.objects
+        .filter(is_active=True)
+        .prefetch_related('images')
+        [:3]
+    )
 
+    return render(
+        request,
+        "index.html",
+        {
+            "featured_rooms":featured_rooms,
+        },
+    )
+
+
+def room_list(request):
+    rooms = (
+        Room.objects
+        .filter(is_active=True)
+        .prefetch_related("images")
+    )
+    
+    return render(
+        request,
+        "core/room_list.html",
+        {
+            "rooms": rooms,
+        }
+    )
+
+
+def room_detail(request, slug):
+    room = get_object_or_404(
+        Room.objects.prefetch_related(
+            "images",
+            "amenities",
+        ),
+        slug=slug,
+    )
+
+    return render(
+        request,
+        "core/room_detail.html",
+        {
+            "room": room,
+        }
+    )
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,13 @@
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
-    path('', include('apps.content.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
+    path("", include("apps.content.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
